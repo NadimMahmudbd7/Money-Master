c8a47eef55b77ec7b01f3972bb31d053ea1d2965
Nadim Mahmud <giftsframebd@gmail.com>
2022-02-16 22:41:34 +0600

    commit message

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const inputIncomeBtn = document.getElementById("inputIncomeBtn")
+
+const inputFoodBtn = document.getElementById("inputFoodBtn")
+const inputFoodBtnValue = parseFloat(inputFoodBtn.value)
+const inputRentBTn = document.getElementById("inputRentBTn")
+const inputRentBTnValue = parseFloat(inputRentBTn.value)
+const inputClothesBtn = document.getElementById("inputClothesBtn")
+const inputClothesBtnValue = parseFloat(inputClothesBtn.value)
+const totalExpense = inputFoodBtnValue + inputRentBTnValue + inputClothesBtnValue
+
+
+document.getElementById("calculate").addEventListener("click",function(){
+    const inputIncomeBtn = document.getElementById("inputIncomeBtn")
+
+})
