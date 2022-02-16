5689c9b98ae99c53f01b25ff0a46ea04a9977aac
Nadim Mahmud <giftsframebd@gmail.com>
2022-02-17 01:33:17 +0600

    half completed

--- js/app.js
+++ js/app.js
@@ -1,15 +1,66 @@
-const inputIncomeBtn = document.getElementById("inputIncomeBtn")
+function totalCost(){
+const inputFoodBtn = document.getElementById("inputFoodBtn");
+const inputFoodBtnText = inputFoodBtn.value;
+const inputFoodBtnValue = parseFloat(inputFoodBtnText);
+const inputRentBTn = document.getElementById("inputRentBTn");
+const inputRentBTnValue = parseFloat(inputRentBTn.value);
+const inputClothesBtn = document.getElementById("inputClothesBtn");
+const inputClothesBtnValue = parseFloat(inputClothesBtn.value);
+let totalExpense = inputFoodBtnValue + inputRentBTnValue + inputClothesBtnValue
+    return totalExpense
+}
 
-const inputFoodBtn = document.getElementById("inputFoodBtn")
-const inputFoodBtnValue = parseFloat(inputFoodBtn.value)
-const inputRentBTn = document.getElementById("inputRentBTn")
-const inputRentBTnValue = parseFloat(inputRentBTn.value)
-const inputClothesBtn = document.getElementById("inputClothesBtn")
-const inputClothesBtnValue = parseFloat(inputClothesBtn.value)
-const totalExpense = inputFoodBtnValue + inputRentBTnValue + inputClothesBtnValue
 
+// For Minus Validation
+function forMinus(inputfield,items){
+    const inputFoodBtn = document.getElementById(inputfield);
+    const inputFoodBtnText = inputFoodBtn.value;
+    const forminuss = document.getElementById(items)
+    if(inputFoodBtn.value<0){
+        forminuss.style.display="block"
+    }
+    else{
+        forminuss.style.display="none"
+    }
+}
+
+
+// Income Balance Field
+function incomeBalance(){
+    const inputIncomeBtn = document.getElementById("inputIncomeBtn");
+    const inputIncomeBtnValue = parseFloat(inputIncomeBtn.value)
+    return inputIncomeBtnValue
+}
 
 document.getElementById("calculate").addEventListener("click",function(){
-    const inputIncomeBtn = document.getElementById("inputIncomeBtn")
+    forMinus("inputIncomeBtn","incomeMinusNumber")
+    forMinus("inputFoodBtn","foodMinusNumber")
+    forMinus("inputRentBTn","rentMinusNumber")
+    forMinus("inputClothesBtn","clothesMinusNumber")
+    if(isNaN(totalCost())){
+        alert('Please Fill all Inputs with Number')
+    }
+    else if(isNaN(incomeBalance())){
+        alert('Please Fill all Inputs with Number')
+    }
+    else{    
+
+const totalExpenses = document.getElementById("totalExpenses");
+const totalExpensesText = totalExpenses.innerText;
+const totalExpensesValue = parseFloat(totalExpensesText);
+const balance = document.getElementById("balance");
+const balanceText = balance.innerText;
+const balanceValue = parseFloat(balanceText)
+const balances = incomeBalance() - totalCost()
+const incomeBalanceLow = document.getElementById("incomeBalanceLow")
+if(incomeBalance() < totalCost()){
+    incomeBalanceLow.style.display = "block"
+}
+else{
+    totalExpenses.innerText = totalCost()
+    balance.innerText = balances
+    incomeBalanceLow.style.display = "none"
+}
+    }
 
 })
